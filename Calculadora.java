6d8427fe92672f75bcaebd53ab0fdea3b8f60bee
FelipeQueiroz <felipeyuhth@users.noreply.github.com>
2020-03-17 19:13:53 -0300

    Add files via upload

---
+++ Calculadora.java
@@ -0,0 +1,26 @@
+package Calculadora;
+
+public class Calculadora {
+   double numeroA;
+   double numeroB;
+   double adicao;
+   double subtracao;
+   double multiplicacao;
+   double divisao;
+   
+   void adicao(){
+	   adicao = numeroA + numeroB;
+   }
+   
+   void subtracao(){
+	   subtracao = numeroA - numeroB;
+   }
+   
+   void multiplicacao(){
+	   multiplicacao = numeroA * numeroB;
+   }
+   
+   void divisao(){
+	   divisao = numeroA / numeroB;
+   }
+}
---
+++ Calculo.java
@@ -0,0 +1,31 @@
+package Calculadora;
+import java.util.Scanner;
+
+public class Calculo {
+
+	public static void main(String[] args) {
+	Scanner in = new Scanner (System.in);	
+		Calculadora Calculo = new Calculadora();
+		double numeroA,numeroB;
+		
+		System.out.println("Digite o primeiro valor:");
+		numeroA = in.nextDouble();
+		
+		System.out.println("Digite o segundo valor:");
+		numeroB = in.nextDouble();
+		
+		Calculo.numeroA = numeroA;
+		Calculo.numeroB = numeroB;
+		Calculo.adicao();
+		Calculo.subtracao();
+		Calculo.multiplicacao();
+		Calculo.divisao();
+		
+		System.out.println(Calculo.numeroA+" + "+Calculo.numeroB+" = "+Calculo.adicao);
+		System.out.println(Calculo.numeroA+" - "+Calculo.numeroB+" = "+Calculo.subtracao);
+		System.out.println(Calculo.numeroA+" x "+Calculo.numeroB+" = "+Calculo.multiplicacao);
+		System.out.println(Calculo.numeroA+" / "+Calculo.numeroB+" = "+Calculo.divisao);
+
+	}
+
+}
